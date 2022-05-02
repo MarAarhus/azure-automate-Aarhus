811621e80764294f784cc7deb706b7d385154767
Martin Aarhus <Martin.Aarhus@nav.no>
2022-05-02 12:07:55 +0200

    oppgave 5-6

---
+++ src/oppgave-6.ps1
@@ -0,0 +1,85 @@
+
+[CmdletBinding()]
+param (
+    # parameter er ikke obligatorisk siden vi har default verdi
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    # når paramater ikke er gitt brukes default verdi
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+
+$kortstokkJson = $webRequest.content
+
+
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value),"
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+#oppg5
+# ...
+
+### Regn ut den samlede poengsummen til kortstokk
+#   Nummererte kort har poeng som angitt på kortet
+#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
+#   Ess (A) teller som 11 poeng
+
+# 1. - utgave - summen av poeng for kort er form for loop/iterere oppgave
+
+$poengKortstokk = 0
+
+# hva er forskjellen mellom -eq, ieg og ceq?
+# # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.2
+
+foreach ($kort in $kortstokk) {
+    if ($kort.value -ceq 'J') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'Q') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'K') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'A') {
+        $poengKortstokk = $poengKortstokk + 11
+    }
+    else {
+        $poengKortstokk = $poengKortstokk + $kort.value
+    }
+}
+
+Write-Host 'Poengsum:' $poengKortstokk
+
+#oppg6
+# ...
+
+
+$meg = $kortstokk[0..1]
+Write-Output "meg: $(kortstokkTilStreng $meg)"
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+$magnus = $kortstokk[0..1]
+Write-Output "magnus: $(kortstokkTilStreng $magnus)"
+$kortstokk = $kortstokk[2..$kortstokk.Count]
+Write-Output "Kortstokk:  $(kortstokkTilStreng $kortstokk)"
