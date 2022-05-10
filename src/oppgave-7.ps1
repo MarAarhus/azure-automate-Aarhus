2b64af7157ec5dc4932338aa74eb95c2f34056a3
Martin Aarhus <Martin.Aarhus@nav.no>
2022-05-10 11:01:34 +0200

    oppg7

---
+++ src/oppgave-7.ps1
@@ -0,0 +1,147 @@
+
+[CmdletBinding()]
+param (
+    # parameter er ikke obligatorisk siden vi har default verdi
+    [Parameter(HelpMessage = "URL til kortstokk", Mandatory = $false)]
+    [string]
+    # når paramater ikke er gitt brukes default verdi
+    $UrlKortstokk = 'http://nav-deckofcards.herokuapp.com/shuffle'
+)
+
+
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri $UrlKortstokk
+
+
+$kortstokkJson = $webRequest.content
+
+
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value),"
+    }
+    return $streng.Substring(0,$streng.Length-1)
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
+
+#oppg5
+# ...
+
+### Regn ut den samlede poengsummen til kortstokk
+#   Nummererte kort har poeng som angitt på kortet
+#   Knekt (J), Dronning (Q) og Konge (K) teller som 10 poeng
+#   Ess (A) teller som 11 poeng
+
+# 1. - utgave - summen av poeng for kort er form for loop/iterere oppgave
+
+$poengKortstokk = 0
+
+# hva er forskjellen mellom -eq, ieg og ceq?
+# # https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.2
+<#
+foreach ($kort in $kortstokk) {
+    if ($kort.value -ceq 'J') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'Q') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'K') {
+        $poengKortstokk = $poengKortstokk + 10
+    }
+    elseif ($kort.value -ceq 'A') {
+        $poengKortstokk = $poengKortstokk + 11
+    }
+    else {
+        $poengKortstokk = $poengKortstokk + $kort.value
+    }
+}
+
+Write-Host 'Poengsum:' $poengKortstokk
+#>
+
+function sumPoengKortstokk {
+    [OutputType([int])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+
+    $poengKortstokk = 0
+
+    foreach ($kort in $kortstokk) {
+        # Undersøk hva en Switch er
+        $poengKortstokk += switch ($kort.value) {
+            { $_ -cin @('J','K','Q') } { 10 }
+            { $_ -cin @('A')} { 11 }
+            default { $kort.value }
+        }
+    }
+    return $poengKortstokk
+}
+
+Write-Output "Poengsum: $(sumPoengKortstokk -kortstokk $kortstokk)"
+
+#oppg6
+# ...
+
+
+$meg = $kortstokk[0..1]
+Write-Output "meg: $(kortstokkTilStreng $meg)"
+$kortstokk = $kortstokk[2..($kortstokk.Count -1)]
+$magnus = $kortstokk[0..1]
+Write-Output "magnus: $(kortstokkTilStreng $magnus)"
+$kortstokk = $kortstokk[2..($kortstokk.Count -1)]
+#Write-Output "Kortstokk:  $(kortstokkTilStreng $kortstokk)"
+
+
+#oppg7
+#...
+
+function skrivUtResultat {
+    param (
+        [string]
+        $vinner,        
+        [object[]]
+        $kortStokkMagnus,
+        [object[]]
+        $kortStokkMeg        
+    )
+    Write-Output "Vinner: $vinner"
+    Write-Output "magnus | $(sumPoengKortstokk -kortstokk $kortStokkMagnus) | $(kortstokkTilStreng -kortstokk $kortStokkMagnus)"    
+    Write-Output "meg    | $(sumPoengKortstokk -kortstokk $kortStokkMeg) | $(kortstokkTilStreng -kortstokk $kortStokkMeg)"
+}
+
+# bruker 'blackjack' som et begrep - er 21
+$blackjack = 21
+
+if (((sumPoengKortstokk -kortstokk $meg) -eq $blackjack ) -AND ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack)) {
+    skrivUtResultat -vinner "draw" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk $magnus) -eq $blackjack ) {
+    skrivUtResultat -vinner "magnus" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+elseif ((sumPoengKortstokk -kortstokk  $meg) -eq $blackjack ) {
+    skrivUtResultat -vinner "meg" -kortStokkMagnus $magnus -kortStokkMeg $meg
+    exit
+}
+
+
+#-UrlKortstokk 'https://azure-gvs-test-cases.azurewebsites.net/api/vinnerDraw'
+#-UrlKortstokk 'https://azure-gvs-test-cases.azurewebsites.net/api/vinnerMagnus'
+#-UrlKortstokk 'https://azure-gvs-test-cases.azurewebsites.net/api/vinnerMeg'
